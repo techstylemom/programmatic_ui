f8366d12877856d98c76aaa2d9f5ae1ec9eaa658
irish <techstylemom@gmail.com>
2021-01-04 10:57:22 +0300

    Initial Commit

---
+++ ProgrammaticUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticUI
+//
+//  Created by Irish on 1/4/21.
+//  Copyright © 2021 Irish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
